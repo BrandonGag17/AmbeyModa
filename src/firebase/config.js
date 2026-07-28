48f1fe13ade01b0bb518996fdc2c2b2dff21d3fa
BrandonProg15 <49434754@est.ort.edu.ar>
2026-07-27 23:18:49 -0300

    empezando

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDm19-GHArO6skcg6uabiDuJBod6xHWBIk",
-  authDomain: "ambey-moda.firebaseapp.com",
-  projectId: "ambey-moda",
-  storageBucket: "ambey-moda.firebasestorage.app",
-  messagingSenderId: "1054443367391",
-  appId: "1:1054443367391:web:336dbab1962079f4a6b26a",
-  measurementId: "G-QGDDC75DE4"
+    apiKey: "AIzaSyDm19-GHArO6skcg6uabiDuJBod6xHWBIk",
+    authDomain: "ambey-moda.firebaseapp.com",
+    projectId: "ambey-moda",
+    storageBucket: "ambey-moda.firebasestorage.app",
+    messagingSenderId: "1054443367391",
+    appId: "1:1054443367391:web:336dbab1962079f4a6b26a",
+    measurementId: "G-QGDDC75DE4"
 };
 
 const app = initializeApp(firebaseConfig);
